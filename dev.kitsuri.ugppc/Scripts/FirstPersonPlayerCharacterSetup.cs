05cc1b54ff7bfddef4a837ac29f717e6d87a4640
Astronix <astronix.gamedev@gmail.com>
2026-01-05 01:04:36 +0530

    Final 0.1

--- dev.kitsuri.ugppc/Scripts/FirstPersonPlayerCharacterSetup.cs
+++ dev.kitsuri.ugppc/Scripts/FirstPersonPlayerCharacterSetup.cs
@@ -1,4 +1,31 @@
-﻿using UnityEngine;
+﻿/*
+ * Copyright 2025 Kitsuri Studios
+ * Developed by Astronix (Porush Ajay Kumar)
+ *
+ * SUMMARY (Apache License 2.0):
+ *  You may use, copy, modify, and distribute this software
+ *  You may use it for commercial and private purposes
+ *  You may sublicense and include it in proprietary projects
+ *  You must include this copyright notice and license text
+ *  You must state significant changes if you modify the code
+ *  You may NOT claim this software as your own original work
+ *  No warranty or liability is provided by the authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+using UnityEngine;
 
 
 namespace UGPPC.dev.kitsuri.ugppc
@@ -82,7 +109,7 @@ namespace UGPPC.dev.kitsuri.ugppc
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-    }
+    }   
 }
 
 }
